c0aa0e6dbaaa640cb744424be4c436c83f287757
cri-cov <cristian.covato@marconibari.edu.it>
2025-04-09 22:15:34 +0000

    CONSEGNA

--- main.cpp
+++ main.cpp
@@ -1,80 +1,60 @@
 #include <iostream>
 using namespace std;
 int main() {
-    
+      int i, n, j, t, v;
+    string s;
     bool scambio;
-    int n, i, f, g, j, t, s;
 
-    cout << "quanti numeri generare?" << endl;
+    cout << "quante macchine hanno gareggiato?" << endl;
     cin >> n;
-    int p[n], d[n], v[n];
+    int r[n];
+    string nomi[n];
 
     i = 0;
     while (i < n) {
-        v[i] = rand() % n;
-        i = i + 1;
-    }
-    i = 0;
-    f = 0;
-    g = 0;
-    while (i < n) {
-        if (v[i] % 2 == 0) {
-            p[f] = v[i];
-            f = f + 1;
-        } else {
-            d[g] = v[i];
-            g = g + 1;
+        cout << "come si chiama la macchina?" << endl;
+        cin >> nomi[i];
+        v = rand() % n + 1;
+        r[i] = v;
+        j = 0;
+        while (j < 1) {
+            if (v == r[i]) {
+                v = v + 1;
+                if (v > n) {
+                    v = 1;
+                }
+                j = 0;
+            } else {
+                j = j + 1;
+            }
         }
+        r[i] = v;
         i = i + 1;
     }
     scambio = true;
     i = 0;
-    while (i <= g - 1 && scambio) {
+    while (i <= n - 1 && scambio) {
         scambio = false;
         j = 0;
-        while (j <= g - 2 - i) {
-            if (d[j] > d[j + 1]) {
-                t = d[j];
-                d[j] = d[j + 1];
-                d[j + 1] = t;
+        while (j <= n - 2 - i) {
+            if (r[j] > r[j + 1]) {
+                t = r[j];
+                r[j] = r[j + 1];
+                r[j + 1] = t;
+                s = nomi[j];
+                nomi[j] = nomi[j + 1];
+                nomi[j + 1] = s;
                 scambio = true;
             }
             j = j + 1;
         }
         i = i + 1;
     }
-    scambio = true;
-    i = 0;
-    while (i <= f - 1 && scambio) {
-        scambio = false;
-        j = 0;
-        while (j <= f - 2 - i) {
-            if (p[j] > p[j + 1]) {
-                t = p[j];
-                p[j] = p[j + 1];
-                p[j + 1] = t;
-                scambio = true;
-            }
-            j = j + 1;
-        }
-        i = i + 1;
-    }
-    i = 0;
-    f = 0;
-    g = 0;
-    while (i < f) {
-        cout << p[i] << endl;
-        i = i + 1;
-    }
-    i = 0;
-    while (i < g) {
-        cout << d[i] << endl;
-        i = i + 1;
-    }
-    i = g;
-    while (i > 0) {
-        cout << d[i] << endl;
-        i = i - 1;
+    j = 0;
+    while (j < n) {
+        cout << "l'auto " << nomi[j] << " è arrivata nella posizione numero " << r[j] << endl;
+        j = j + 1;
     }
     return 0;
 }
+
